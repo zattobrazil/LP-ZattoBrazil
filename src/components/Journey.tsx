94fd16bd8ddf30141a9994e442afb1bd7dee9ef0
Murilo <murilosilveira.pl017@academico.ifsul.edu.br>
2026-03-23 11:13:12 -0300

    fix: 3

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -8,7 +8,7 @@ export default function Journey() {
       <div className="relative max-w-7xl mx-auto aspect-[21/9] overflow-visible">
         {/* Imagem do infográfico */}
         <Image
-          src="/images/jornada.png"
+          src="/images/jornada.png?v=novo" 
           alt="Gráfico da Jornada do Cliente"
           width={1920}
           height={800}
